691606fd2cf640c54996ab367760f1b47600ae2a
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-05-05 18:41:35 -0500

    Backend - NestJs - Table products - 007

--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,47 @@
-export class Product {}
+import { User } from "src/users/entities/user.entity";
+import { Column, Entity, ManyToMany, ManyToOne, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("products")
+export class Product {
+    @PrimaryGeneratedColumn("uuid")
+    id: string
+
+    @Column({
+        type: "varchar",
+        length: 100,
+        unique: true,
+        nullable: false
+    })
+    name: string
+
+    @Column({
+        type: "varchar",
+        length: 200,
+        nullable: false
+    })
+    description: string
+
+    @Column({
+        type: "decimal",
+        precision: 10,
+        scale: 2,
+        nullable: false
+    })
+    price: number
+
+    @Column({
+        type: "int",
+        nullable: false
+    })
+    stock: number
+
+    @Column({
+        type: "varchar",
+        length: 200,
+        nullable: false
+    })
+    imgUrl: string
+
+    @ManyToOne(() => User, (user) => user.products)
+    user: User;
+}
