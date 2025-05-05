1476a09cba76fa9dc3aa953373e4cc19d3c3732c
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2025-05-05 18:41:56 -0500

    Backend - NestJs - Table users - 007

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,59 @@
-export class User {}
+import { Product } from "src/products/entities/product.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity("users")
+export class User {
+
+    @PrimaryGeneratedColumn("uuid")
+    id: string;
+
+    @Column({
+        type: "varchar",
+        length: 100,
+        unique: true,
+        nullable: false
+    })
+    name: string;
+
+    @Column({
+        type: "varchar",
+        length: 100,
+        unique: true,
+        nullable: false
+    })
+    email: string;
+
+    @Column({
+        type: "varchar",
+        length: 100,
+        unique: true,
+        nullable: false,
+        select: false
+    })
+    password: string;
+
+    @Column({
+        type: "varchar",
+        length: 100,
+        unique: true,
+        nullable: false
+    })
+    phone: string;
+
+    @Column({
+        type: "varchar",
+        length: 100,
+        nullable: false
+    })
+    country: string;
+
+    @Column({
+        type: "varchar",
+        length: 100,
+        nullable: false
+    })
+    city: string;
+
+    @OneToMany(() => Product, (product) => product.user)
+    products: Product[];
+}
