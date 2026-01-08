15fbd848bc1f47121d425e9ed8f93d239b91df3f
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-01-07 19:05:03 -0500

    Se añade logica de seeds module en main

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { loggerGlobal } from './middleware/logger/logger.middleware';
+import { UserSeed } from './seeds/users/module-seeds.users';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.use(loggerGlobal);
+
+  const userSeed = app.get(UserSeed);
+  await userSeed.seedUser();
+  console.log('*** LA INSERCION DE USUARIOS FUE EXITOSA ***');
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
