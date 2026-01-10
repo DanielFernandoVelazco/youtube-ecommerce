13dc1c8337a518ade8f9c940518be25ccdb7d011
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-01-10 00:47:13 -0500

    Backend - NestJs - TypeORM SEEDS products - 011

--- src/seeds/seed.module.ts
+++ src/seeds/seed.module.ts
@@ -2,11 +2,13 @@ import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { User } from "src/users/entities/user.entity";
 import { UserSeed } from "./users/module-seeds.users";
+import { ProductSeed } from "./products/module-seeds.products";
+import { Product } from "src/products/entities/product.entity";
 
 @Module({
-    imports: [TypeOrmModule.forFeature([User])],
-    providers: [UserSeed],
-    exports: [UserSeed]
+    imports: [TypeOrmModule.forFeature([User, Product])],
+    providers: [UserSeed, ProductSeed],
+    exports: [UserSeed, ProductSeed],
 })
 
 export class SeedModule { }
