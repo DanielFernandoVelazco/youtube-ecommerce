8a0f832d687e5a735050380e1f0efd6be044b130
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-01-10 00:46:55 -0500

    Backend - NestJs - TypeORM SEEDS products - 011

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { loggerGlobal } from './middleware/logger/logger.middleware';
 import { UserSeed } from './seeds/users/module-seeds.users';
+import { ProductSeed } from './seeds/products/module-seeds.products';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,10 @@ async function bootstrap() {
   const userSeed = app.get(UserSeed);
   await userSeed.seedUser();
   console.log('*** LA INSERCION DE USUARIOS FUE EXITOSA ***');
+
+  const productSeed = app.get(ProductSeed);
+  await productSeed.seedProduct();
+  console.log('*** LA INSERCION DE PRODUCTOS FUE EXITOSA ***');
 
   await app.listen(process.env.PORT ?? 3000);
 }
