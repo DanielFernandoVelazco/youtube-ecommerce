8ce8fab94f2f91a4644e20dccb1f73b2534ad370
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-01-18 23:21:49 -0500

    Se modifica module auth

--- src/auths/auths.module.ts
+++ src/auths/auths.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AuthsService } from './auths.service';
 import { AuthsController } from './auths.controller';
+import { UsersModule } from '../users/users.module';
+import { SimpleAuthGuard } from './guards/simple-auth.guard';
 
 @Module({
+  imports: [UsersModule],
   controllers: [AuthsController],
-  providers: [AuthsService],
+  providers: [AuthsService, SimpleAuthGuard],
+  exports: [AuthsService, SimpleAuthGuard],
 })
-export class AuthsModule {}
+export class AuthsModule { }
