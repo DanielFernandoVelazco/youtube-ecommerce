6f9bd0f1011767a514fd654e48d02049b770ca79
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-01-18 23:25:22 -0500

    Se incluye logica de auth

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -36,4 +36,16 @@ export class UsersService {
   async remove(id: string) {
     return await this.userRepository.delete(id);
   }
+
+  async findByEmailWithPassword(email: string): Promise<User | null> {
+    return await this.userRepository
+      .createQueryBuilder('user')
+      .where('user.email = :email', { email })
+      .addSelect('user.password')
+      .getOne();
+  }
+
+  async findById(id: string): Promise<User | null> {
+    return await this.userRepository.findOneBy({ id });
+  }
 }
