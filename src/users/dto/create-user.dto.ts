1d94da1991869f7045ffc867a80798b4b306000f
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-01-18 23:25:59 -0500

    Se modifica password en la logica

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -7,12 +7,7 @@ export class CreateUserDto {
     @IsEmail()
     email: string
 
-    @Matches(
-        /((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/,
-        {
-            message: 'The password must have a Uppercase, lowercase letter and a number'
-        }
-    )
+    @IsString()
     password: string
 
     @IsString()
